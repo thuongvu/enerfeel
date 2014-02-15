267702b12b7bc3c46650a7e8f737cd372761740f
thuongvu <thuongvuho@gmail.com>
2014-02-15 00:13:59 -0800

    filter "working". switching works. day looks ugly with a vertical line.  week's has a line on the first one going back

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('app.controllers', [])
 	.controller('mainCtrl', ['$scope', function ($scope) {
 		console.log("in mainCtrl")
 
-		$scope.dataContainer = [{"date": new Date(2014, 1, 13, 15), "energylevel":2, "note":"hello"},{"date": new Date(2014, 1, 13, 18), "energylevel":4, "note": "world"}, {"date": new Date(2014, 1, 14, 19), "energylevel":3, "note":"this is cool stuff"}];
+		$scope.dataContainer = [{"date": new Date(2014, 1, 13, 15), "energylevel":2, "note":"hello"},{"date": new Date(2014, 1, 14, 18), "energylevel":4, "note": "world"}, {"date": new Date(2014, 1, 14, 19), "energylevel":3, "note":"this is cool stuff"}];
 		console.log($scope.dataContainer)
 		$scope.energy = {};
 		var counter = 4;
@@ -27,23 +27,40 @@ angular.module('app.controllers', [])
 			$scope.energy.note = null;
 		}
 
+		$scope.dataContainerTwo = $scope.dataContainer;
 		$scope.filter = function(time) {
 			console.log(time);
+
+			$scope.dataContainerTwo = []; // empty out dataContainerTwo
+
+			if (time === 'day') {
+				var timeAmount = Date.now() - 86400000;
+			} else if (time === 'week') {
+				var timeAmount = Date.now() - 604800000;
+			} else if (time === 'month') {
+				var timeAmount = Date.now() - 2.62974e9;
+			}
+
+			for (prop in $scope.dataContainer) {
+				var obj = $scope.dataContainer;
+				var dateOfProp = obj[prop].date.valueOf();
+				if (dateOfProp > timeAmount) {
+					$scope.dataContainerTwo.push(obj[prop])
+				}
+			}
+			console.log($scope.dataContainerTwo)
+
 		}
 
-		// $scope.dataContainerTwo = [];
+		
 
 		// console.log("ms right now " + Date.now())
 		// var yesterdayInMs = Date.now() - 86400000;
 		// for (prop in $scope.dataContainer) {
 		// 	var obj = $scope.dataContainer;
 		// 	var dateOfProp = obj[prop].date.valueOf();
-		// 	// console.log(obj[prop].date)
-		// 	// console.log(obj[prop].date.valueOf())
 		// 	if (dateOfProp < yesterdayInMs) {
 		// 		$scope.dataContainerTwo.push(obj[prop])
-		// 		// delete obj[prop]
-		// 		// console.log(obj)
 		// 	}
 		// }
 		// console.log($scope.dataContainerTwo)
