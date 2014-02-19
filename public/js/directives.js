2cb9b11973a8e89215088574896f3053082cad0a
thuongvu <thuongvuho@gmail.com>
2014-02-18 21:56:37 -0800

    new directive for showing 'add'

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,22 @@
 angular.module('app.directives', [])
+	.directive('add', ['EventService', 'FilterService', function (EventService, FilterService) {
+		return {
+			restrict: 'EA',
+			// scope: {},
+			controller: function ($scope) {
+				$scope.addTest = function() {
+					console.log("addTest")
+					if ($scope.showAdd === true) {
+						$scope.showAdd = false; 
+					} else {
+						$scope.showAdd = true;
+					}
+				}
+				$scope.showAdd = false;
+			},
+			link: function (scope, iElement, iAttrs) {
+
+			},
+			template: '<button ng-click=' + "addTest()" + '>ADD</button><span ng-show=' + "showAdd" + '>This is sometimes hidden</span>'
+		}
+	}])
