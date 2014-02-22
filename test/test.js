c4c3f0a8087f66671122aa187e10cad1a36cd269
thuongvu <thuongvuho@gmail.com>
2014-02-21 19:26:07 -0800

    added a services unit test

--- test/test.js
+++ test/test.js
@@ -1,55 +1,71 @@
-describe('Testing Modules:', function() {
-	describe("The 'app' module", function () {
-		var myApp;
-		beforeEach(function() {
-			myApp = angular.module("app");
-		})	
+// describe('Testing Modules:', function() {
+// 	describe("The 'app' module", function () {
+// 		var myApp;
+// 		beforeEach(function() {
+// 			myApp = angular.module("app");
+// 		})	
 		
-		it("should be registered", function() {
-			expect(myApp).toBeDefined();
-		});
+// 		it("should be registered", function() {
+// 			expect(myApp).toBeDefined();
+// 		});
 
-		describe("Dependencies:", function() {
-			var dependencies;
-			beforeEach(function() {
-				dependencies = myApp.value('app').requires;
-			})
+// 		describe("Dependencies:", function() {
+// 			var dependencies;
+// 			beforeEach(function() {
+// 				dependencies = myApp.value('app').requires;
+// 			})
 
-			var hasModule = function (m) {
-				return dependencies.indexOf(m) >=0;
-			};
+// 			var hasModule = function (m) {
+// 				return dependencies.indexOf(m) >=0;
+// 			};
 
-			it("should have app.controllers as a dependency", function() {
-				expect(hasModule('app.controllers')).toBeGreaterThan(0);
-			})
+// 			it("should have app.controllers as a dependency", function() {
+// 				expect(hasModule('app.controllers')).toBeGreaterThan(0);
+// 			})
+// 		});
 
-		})
+// 	})
+// })
 
-		
-	})
-})
+// describe("Unit: mainCtrl", function() {
 
-
-describe("Unit: mainCtrl", function() {
-
-	beforeEach(function() {
-		module("app");
-	})	
+// 	beforeEach(function() {
+// 		module("app");
+// 	})	
 	
-	var ctrl, scope;
+// 	var ctrl, scope;
 
-	beforeEach(inject(function($controller, $rootScope) {
-		scope = $rootScope.$new();
+// 	beforeEach(inject(function($controller, $rootScope) {
+// 		scope = $rootScope.$new();
 
-		ctrl = $controller('mainCtrl', {
-			$scope: scope
-		});
-	}))
+// 		ctrl = $controller('mainCtrl', {
+// 			$scope: scope
+// 		});
+// 	}))
 
-	it("should create $scope.greeting when calling sayHello", function() {
-		expect(scope.greeting).toBeUndefined();
-		scope.sayHello();
-		expect(scope.greeting).toEqual("Hello Thuongvu");
+// 	it("should create $scope.greeting when calling sayHello", function() {
+// 		expect(scope.greeting).toBeUndefined();
+// 		scope.sayHello();
+// 		expect(scope.greeting).toEqual("Hello Thuongvu");
+// 	})
+// })
+
+describe('Services:', function() {
+	beforeEach(module('app'))
+
+	describe('EventService', function() {
+		var EventService;
+		beforeEach(inject(function($injector) {
+			EventService = $injector.get('EventService')
+		}))
+
+		it('should not be null', function() {
+			expect(EventService).not.toBeNull()
+		})
 	})
+	
 })
+	
+
+
 
