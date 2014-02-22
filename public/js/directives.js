98b5a914f0ac88dbff2bd9fd2f5435009e0504f1
thuongvu <thuongvuho@gmail.com>
2014-02-22 15:03:51 -0800

    cleared away old stuff in directive for Add, refactored my addEvent function

--- public/js/directives.js
+++ public/js/directives.js
@@ -28,8 +28,6 @@ var AddController = function ($scope, $timeout) {
 		$scope.input.opacity = $scope.input.checkbox.checked;
 	}
 
-	
-
 	function catchEmptyInputs() {
 		if (($scope.input.opacity == undefined) || ($scope.input.opacity == 0)) {
 			$scope.input.opacity = 3;
@@ -59,21 +57,19 @@ var AddController = function ($scope, $timeout) {
 
 	}
 
-	// ADD LOGIC
-	$scope.addEvent = function(energyLevel, note, category) {
-		
-		console.log($scope.eventService.allLifeEvents);			
+	function pushDataIntoServices(eventData) {
+		$scope.eventService.allLifeEvents.push(eventData);
+		$scope.lifeEventsInView.push(eventData)
+		$scope.filterService.sortTime($scope.lifeEventsInView);
+	}
 
-		if (category === 'meal') {
-			addIfMeal();
+	function clearInputs() {
+		for (prop in $scope.input) {
+			$scope.input[prop] = null;
 		}
+	}
 
-		if (category === 'sleep') {
-			addIfSleep(energyLevel, note, category);
-		}
-
-		catchEmptyInputs();
-
+	function createEventDataObj(energyLevel, note, category) {
 		var eventData = {
 			energylevel : energyLevel,
 			note			: note,
@@ -82,26 +78,28 @@ var AddController = function ($scope, $timeout) {
 			opacity		: $scope.input.opacity,
 			size			: $scope.input.size
 		};
+		return eventData;
+	}
 
-		$scope.eventService.allLifeEvents.push(eventData);
-		$scope.lifeEventsInView.push(eventData)
-		$scope.filterService.sortTime($scope.lifeEventsInView);
-
-		$scope.showAdd = false;
-
-		for (prop in $scope.input) {
-			$scope.input[prop] = null;
+	// ADD LOGIC
+	$scope.addEvent = function(energyLevel, note, category) {
+		
+		if (category === 'meal') {
+			addIfMeal();
+		} else if (category === 'sleep') {
+			addIfSleep(energyLevel, note, category);
 		}
 
+		catchEmptyInputs();
+		var eventData = createEventDataObj(energyLevel, note, category);
+		pushDataIntoServices(eventData);
+		$scope.showAddFunc();
+		clearInputs();
 		showHideCategories('all');
-
-		console.log($scope.eventService.allLifeEvents);					
-
 	};
 
 	// DATEPICKER LOGIC
 	$scope.dateTimePicked = new Date();
-	// $scope.sleepTime = new Date();
 	$scope.wakeTime = new Date();
 	setTime();
 
@@ -137,7 +135,6 @@ var AddController = function ($scope, $timeout) {
 				$scope.show[category] = true;
 			}
 		}
-		// console.log($scope.show)
 	}
 
 	$scope.show.meal = false;
@@ -159,163 +156,8 @@ angular.module('app.directives', [])
 		return {
 			restrict: 'EA',
 			// scope: {},
-			// controller: function ($scope) {
-			// 	$scope.showAdd = false;
-			// 	$scope.show = {};
-			// 	$scope.input.checkbox = {};
-			// 	$scope.input.checkbox.checked = 0;
-
-			// 	// show the ADD div or not
-			// 	$scope.showAddFunc = function() {
-			// 		if ($scope.showAdd === true) {
-			// 			$scope.showAdd = false; 
-			// 		} else {
-			// 			$scope.showAdd = true;
-			// 		}
-			// 	};
-
-			// 	function addIfMeal() {
-			// 		if ($scope.input.checkbox == null) {
-			// 			$scope.input.checkbox = {};
-			// 		}
-			// 		$scope.input.checkbox.checked = 0; 
-
-			// 		for (food in $scope.input.checkbox) {
-			// 			if ($scope.input.checkbox[food] === true) {
-			// 				$scope.input.checkbox.checked++;
-			// 			}
-			// 		};
-			// 		$scope.input.opacity = $scope.input.checkbox.checked;
-			// 	}
-
-			
-
-			// 	function catchEmptyInputs() {
-			// 		if (($scope.input.opacity == undefined) || ($scope.input.opacity == 0)) {
-			// 			$scope.input.opacity = 3;
-			// 		}
-			// 		if (($scope.input.size == undefined) || ($scope.input.size == 0)) {
-			// 			if ($scope.input.category = 'exercise') {
-			// 				$scope.input.size = 30;
-			// 			} else {
-			// 				$scope.input.size = 3;
-			// 			}
-			// 		}					
-			// 	}
-
-			// 	function addIfSleep(energyLevel, note, category) {
-			// 		var eventData = {
-			// 			energylevel : energyLevel,
-			// 			note			: note,
-			// 			date        : $scope.wakeTime,
-			// 			category 	: category,	
-			// 			opacity		: $scope.input.opacity,
-			// 			size			: $scope.input.size
-			// 		};
-
-			// 		$scope.eventService.allLifeEvents.push(eventData);
-			// 		$scope.lifeEventsInView.push(eventData)
-
-			// 	}
-
-			// 	// ADD LOGIC
-			// 	$scope.addEvent = function(energyLevel, note, category) {
-					
-			// 		console.log($scope.eventService.allLifeEvents);			
-
-			// 		if (category === 'meal') {
-			// 			addIfMeal();
-			// 		}
-
-			// 		if (category === 'sleep') {
-			// 			addIfSleep(energyLevel, note, category);
-			// 		}
-
-			// 		// console.log("$scope.input.opacity is " + $scope.input.opacity);
-			// 		// console.log("$scope.input.size is " + $scope.input.size);
-			// 		catchEmptyInputs();
-
-			// 		var eventData = {
-			// 			energylevel : energyLevel,
-			// 			note			: note,
-			// 			date        : $scope.dateTimePicked,
-			// 			category 	: category,	
-			// 			opacity		: $scope.input.opacity,
-			// 			size			: $scope.input.size
-			// 		};
-
-			// 		$scope.eventService.allLifeEvents.push(eventData);
-			// 		$scope.lifeEventsInView.push(eventData)
-			// 		$scope.filterService.sortTime($scope.lifeEventsInView);
-
-			// 		$scope.showAdd = false;
-
-			// 		for (prop in $scope.input) {
-			// 			$scope.input[prop] = null;
-			// 		}
-
-			// 		showHideCategories('all');
-
-			// 		console.log($scope.eventService.allLifeEvents);					
-
-			// 	};
-
-			// 	// DATEPICKER LOGIC
-			// 	$scope.dateTimePicked = new Date();
-			// 	// $scope.sleepTime = new Date();
-			// 	$scope.wakeTime = new Date();
-			// 	setTime();
-
-			// 	var timeIntervalFunction = function() {
-			// 		cancelRefresh = $timeout(function createNewDateObj() {
-			// 			$scope.dateTimePicked = new Date();
-			// 			// console.log("the new time is " + $scope.dateTimePicked);
-			// 			cancelRefresh = $timeout(createNewDateObj, 60000);
-			// 		}, 60000);
-			// 	};
-
-			// 	function setTime() {
-			// 		var currentTime = new Date();
-			// 		var currentTimeSeconds = currentTime.getSeconds();
-			// 		var secsUntilNextMin = (60 - currentTimeSeconds) * 1000;
-			// 		// console.log("currentTimeSeconds is " + currentTimeSeconds + " secsUntilNextMin " + secsUntilNextMin);
-			// 		$timeout(function() {
-			// 			$scope.dateTimePicked = new Date();
-			// 			timeIntervalFunction()
-			// 		}, secsUntilNextMin)
-			// 	};
-
-			// 	$scope.$on('destroy', function(e) {
-			// 		$timeout.cancel(cancelRefresh);
-			// 	});
-
-			// 	// CATEGORIES SHOW/HIDE + LOGIC
-			// 	function showHideCategories(cat) {
-			// 		for (category in $scope.show) {
-			// 			if (category !== cat) {
-			// 				$scope.show[category] = false;
-			// 			} else {
-			// 				$scope.show[category] = true;
-			// 			}
-			// 		}
-			// 		// console.log($scope.show)
-			// 	}
-
-			// 	$scope.show.meal = false;
-			// 	$scope.show.exercise = false;
-			// 	$scope.show.work = false;
-			// 	$scope.show.sleep = false;
-
-			// 	function watchCategory() {
-			// 		showHideCategories($scope.input.category);
-			// 	}
-
-			// 	$scope.$watch('input.category', watchCategory, true )
-
-			// },
 			controller: AddController,
 			link: function (scope, iElement, iAttrs) {
-
 			},
 			templateUrl: 'directiveTemplates/addTemplate.html'
 		}
