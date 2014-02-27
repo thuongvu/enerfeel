e26f8cfbedea6f8bbe4ac94616ebdb3a7fbc9219
thuongvu <thuongvuho@gmail.com>
2014-02-26 18:34:43 -0800

    i can get data from mongodb now on a /get request

--- server.js
+++ server.js
@@ -25,24 +25,28 @@ app.get("/", function(req,res) {
 })
 
 app.get("/get", function (req, res) {
-	var sampleData = [
-	 {"date": new Date(2014, 0, 13, 15), "energylevel":3, "note":"last month, ate food", "category": "meal", "opacity": 1, "size": 1},
-	 {"date": new Date(2014, 1, 13, 15), "energylevel":2, "note":"ate more food", "category": "meal", "opacity": 2, "size": 2},
-	 {"date": new Date(2014, 1, 14, 18), "energylevel":4, "note": "ran", "category": "exercise", "opacity": 3, "size": 5}, 
-	 {"date": new Date(2014, 1, 15, 19), "energylevel":3, "note":"swam", "category": "exercise", "opacity": 4, "size": 15},
-	 {"date": new Date(2014, 1, 16, 4), "energylevel":1, "note":"ate snack", "category": "exercise", "opacity": 5, "size":10},
-	 {"date": new Date(2014, 1, 16, 15), "energylevel":4, "note":"ate snack", "category": "meal", "opacity": 1, "size": 5},
-	 {"date": new Date(), "energylevel":4, "note":"slept for a long time", "category": "sleep", "opacity": 1, "size": 10},
-	 {"date": new Date(), "energylevel":5, "note":"ate sensu bean", "category": "meal", "opacity": 4, "size": 5}
-	 ];
-	res.json(sampleData);
+	db.lifeEventsCollection.find(function(err, data) {
+		console.log(data);
+		res.json(data);
+	})
+	// var sampleData = [
+	//  {"date": new Date(2014, 0, 13, 15), "energylevel":3, "note":"last month, ate food", "category": "meal", "opacity": 1, "size": 1},
+	//  {"date": new Date(2014, 1, 13, 15), "energylevel":2, "note":"ate more food", "category": "meal", "opacity": 2, "size": 2},
+	//  {"date": new Date(2014, 1, 14, 18), "energylevel":4, "note": "ran", "category": "exercise", "opacity": 3, "size": 5}, 
+	//  {"date": new Date(2014, 1, 15, 19), "energylevel":3, "note":"swam", "category": "exercise", "opacity": 4, "size": 15},
+	//  {"date": new Date(2014, 1, 16, 4), "energylevel":1, "note":"ate snack", "category": "exercise", "opacity": 5, "size":10},
+	//  {"date": new Date(2014, 1, 16, 15), "energylevel":4, "note":"ate snack", "category": "meal", "opacity": 1, "size": 5},
+	//  {"date": new Date(), "energylevel":4, "note":"slept for a long time", "category": "sleep", "opacity": 1, "size": 10},
+	//  {"date": new Date(), "energylevel":5, "note":"ate sensu bean", "category": "meal", "opacity": 4, "size": 5}
+	//  ];
+	// res.json(sampleData);
 });
 
 app.post('/post', function (req, res) {
 	console.log(req.body);
 	// remember to sanitize this object!!!!!
 	var obj = req.body;
-	db.lifeEventsCollection.save({lifeEvent: obj}, function(err, saved) {
+	db.lifeEventsCollection.save(obj, function(err, saved) {
 		if (err || !saved) {
 			console.log("didnt save");
 			res.send(200, "SHIT");
