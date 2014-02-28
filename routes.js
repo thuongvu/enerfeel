7f0d40c1e61aba12a2b0172e7ebe8e41a9654eec
thuongvu <thuongvuho@gmail.com>
2014-02-27 23:04:51 -0800

    added passport.js facebook strategy to routes.js

--- routes.js
+++ routes.js
@@ -1,15 +1,23 @@
 module.exports = function(app, passport) {
-	app.get('/auth', passport.authenticate('fb'));
+	app.get('/auth', passport.authenticate('facebook'));
 
-	// app.get('/auth/callback', passport.authenticate('fb', function(req, res) {
-	// }));
+	// this is my way, because i need to do it this way for an SPA
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', function(req, res) {
+			console.log("user authenticated")
+			console.log("mongo query")
+			res.send({"send": "user data"});
+			// make a mongodb query w/ id
+			// send user data
+	}));
 
-	// app.get('/logout', function (req, res) {
-	// 	req.logout();
-	// 	res.redirect('/');
-	// });
+	
 
-	// // i don't think i need this one
+	app.get('/logout', function (req, res) {
+		req.logout();
+		res.redirect('/'); // can i even do that in an spa? more likely that i send a json w/ data
+	});
+
+	// // i don't think i need this one because it's an spa. i'll deal with it clientside?
 	// function isLoggedIn(req, res, next) {
 	// 	if (req.isAuthenticated()) {
 	// 		return next();
