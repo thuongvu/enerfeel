f0bc085b44c1ebd8b3d37dce88c10b540acce300
thuongvu <thuongvuho@gmail.com>
2014-03-03 17:19:58 -0800

    cleaned up a little bit

--- routes.js
+++ routes.js
@@ -7,44 +7,27 @@ module.exports = function(app, passport) {
 	// this is my way, because i need to do it this way for an SPA
 	app.get('/auth/facebook/callback', 
 		passport.authenticate('facebook'), function(req, res) {
-			console.log("user authenticated");
-			console.log("mongo query");
-			// console.log("req.user");
+			console.log("req.user");
 			console.log(req.user);
-			console.log("res")
-			// console.log(res);
-
-			// res.redirect('/')
 			res.cookie('user', JSON.stringify({
 				'token': req.user.token,
 				'authLevel': 1,
 			}));
 
 			res.redirect('/');
-			res.send("blah")
-			// console.log("res");
-			// console.log(res);
-			// res.redirect('/');
-			// res.json({"send": "user data"});
-			// console.log("res");
-			// console.log(res);
-			// res.send({"send": "user data"});
-			// make a mongodb query w/ id
-			// send user data
 	});
 
 	app.get('/get', function(req, res) {
 		console.log("req.headers");
 		console.log(req.headers);
-		db.users.findOne({'token' : req.headers.token}, function(err, data) {
-			console.log("user Data");
-			console.log(data);
-			res.send(data.lifeEvents);
-		});
-		// User.findOne({'token' : req.headers.token}, function(err, user) {
-		// 	console.log(user);
-		// });
-	})
+		if (req.headers.token !== 'null') {
+			db.users.findOne({'token' : req.headers.token}, function(err, data) {
+				console.log("user Data");
+				console.log(data);
+				res.send(data.lifeEvents);
+			});
+		};
+	});
 
 
 	app.get('/logout', function (req, res) {
