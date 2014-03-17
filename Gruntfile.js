3004ad2f5d1ccb06152a417c3b96e759bf1c955b
thuongvu <thuongvuho@gmail.com>
2014-03-16 19:32:51 -0700

    file concat works

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,49 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    // pkg: grunt.file.readJSON('./public/bower.json'),
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [
+        // 'public/js/*.js', 
+        'public/bower_components/jquery/jquery.min.js', 
+        'public/bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
+        'public/bower_components/angular/angular.min.js', 
+        'public/bower_components/angular-cookies/angular-cookies.min.js', 
+        'public/bower_components/angular-resource/angular-resource.min.js',
+        'public/bower_components/angular-route/angular-route.min.js',
+        'public/bower_components/ngQuickDate/dist/ng-quick-date.js',
+         'public/bower_components/d3module.js',
+         'public/build/uglified.js'
+        ],
+        dest: 'public/build/concat.js'
+      }
+    },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        compress: {
+          drop_console: true
+        },
+        preserveComments: false
       },
       build: {
-        src: ['public/js/*.js'],
-        dest: 'public/build/<%= pkg.name %>.min.js'
+        src: [
+            'public/js/*.js', 
+            'public/bower_components/angular-ui-slider/src/slider.js', 
+            'public/bower_components/dropdown.js',
+        ],
+        // dest: 'public/build/<%= pkg.name %>.min.js'
+        dest: 'public/build/uglified.js'
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load Plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  // Tasks to run
+  grunt.registerTask('default', ['uglify', 'concat']);
 
 };
